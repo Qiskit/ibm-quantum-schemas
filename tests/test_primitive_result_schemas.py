81637d6683aeb317d495371fc3064a7372569ef2
gadial <gadial@gmail.com>
2024-05-06 16:36:14 +0300

    Basic estimator result schema + test

--- tests/test_primitive_result_schemas.py
+++ tests/test_primitive_result_schemas.py
@@ -30,7 +30,9 @@ class TestEstimatorV2ResultSchema(unittest.TestCase):
         self.validator = jsonschema.Draft202012Validator(schema=self.estimator_schema)
 
     def test_basic_result(self):
-        result = '{"results": [{"data": {"evs": 1.1142546245919478, "stds": 0.012101383035893986, "ensemble_standard_error": 0.012101383035893986}, "metadata": {"shots": 4096, "target_precision": 0.015625, "circuit_metadata": {}, "resilience": {}, "num_randomizations": 32}}], "metadata": {"dynamical_decoupling": {"enable": false, "sequence_type": "XX", "extra_slack_distribution": "middle", "scheduling_method": "alap"}, "twirling": {"enable_gates": false, "enable_measure": true, "num_randomizations": "auto", "shots_per_randomization": "auto", "interleave_randomizations": true, "strategy": "active-accum"}, "resilience": {"measure_mitigation": true, "zne_mitigation": false, "pec_mitigation": false}, "version": 2}}'
+        result_str = '{"results": [{"data": {"evs": 1.1142546245919478, "stds": 0.012101383035893986, "ensemble_standard_error": 0.012101383035893986}, "metadata": {"shots": 4096, "target_precision": 0.015625, "circuit_metadata": {}, "resilience": {}, "num_randomizations": 32}}], "metadata": {"dynamical_decoupling": {"enable": false, "sequence_type": "XX", "extra_slack_distribution": "middle", "scheduling_method": "alap"}, "twirling": {"enable_gates": false, "enable_measure": true, "num_randomizations": "auto", "shots_per_randomization": "auto", "interleave_randomizations": true, "strategy": "active-accum"}, "resilience": {"measure_mitigation": true, "zne_mitigation": false, "pec_mitigation": false}, "version": 2}}'
+        result = json.loads(result_str)
+        self.validator.validate(result)
 
 class TestSamplerV2ResultSchema(unittest.TestCase):
     """Tests the sampler result schema"""
@@ -41,4 +43,7 @@ class TestSamplerV2ResultSchema(unittest.TestCase):
         self.validator = jsonschema.Draft202012Validator(schema=self.sampler_schema)
 
     def test_basic_result(self):
-        result = '{"results": [{"data": {"meas": {"samples": ["0x3", "0x3", "0x3", "0x3", "0x3"], "num_bits": 2}}, "metadata": {"circuit_metadata": {}}}], "metadata": {"version": 2}}'
+        result_str = '{"results": [{"data": {"meas": {"samples": ["0x3", "0x3", "0x3", "0x3", "0x3"], "num_bits": 2}}, "metadata": {"circuit_metadata": {}}}], "metadata": {"version": 2}}'
+        result = json.loads(result_str)
+        print(result)
+        self.validator.validate(result)
