94191105254fefdee56b77532c78546dc5650054
Ian Hincks <ian.hincks@gmail.com>
2025-11-03 09:34:51 -0500

    Add execution spans to the executor model (#23)
    * commiteroo
    
    * update and test
    
    * change format

--- ibm_quantum_schemas/models/executor/version_0_1/models.py
+++ ibm_quantum_schemas/models/executor/version_0_1/models.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+import datetime
 from typing import Annotated, Literal, Optional, Union
 
 from pydantic import BaseModel, Field, model_validator
@@ -152,6 +153,45 @@ class QuantumProgramResultItemModel(BaseModel):
     """Metadata pertaining to the execution of this particular quantum program item."""
 
 
+class ChunkPart(BaseModel):
+    """A description of the contents of a single part of an execution chunk."""
+
+    idx_item: int
+    """The index of an item in a quantum program."""
+
+    size: int
+    """The number of elements from the quantum program item that were executed.
+
+    For example, if a quantum program item has shape ``(10, 5)``, then it has a total of ``50``
+    elements, so that if this ``size`` is ``10``, it constitutes 20% of the total work for the item.
+    """
+
+
+class ChunkSpan(BaseModel):
+    """Timing information about a single chunk of execution.
+
+    .. note::
+
+        This span may include some amount of non-circuit time.
+    """
+
+    start: datetime.datetime
+    """The start time of the execution chunk in UTC."""
+
+    stop: datetime.datetime
+    """The stop time of the execution chunk in UTC."""
+
+    parts: list[ChunkPart]
+    """A description of which parts of a quantum program are contained in this chunk."""
+
+
+class MetadataModel(BaseModel):
+    """Execution metadata."""
+
+    chunk_timing: list[ChunkSpan]
+    """Timing information about all executed chunks of a quantum program."""
+
+
 class QuantumProgramResultModel(BaseModel):
     """Result from executing a quantum program."""
 
@@ -161,5 +201,5 @@ class QuantumProgramResultModel(BaseModel):
     data: list[QuantumProgramResultItemModel]
     """Resulting data for each quantum program item."""
 
-    metadata: None
+    metadata: MetadataModel
     """Execution metadata pertaining to the job as a whole."""
--- test/models/executor/version_0_1/test_models.py
+++ test/models/executor/version_0_1/test_models.py
@@ -12,13 +12,18 @@
 
 """Tests for 0.1 models."""
 
+import datetime
+
 import numpy as np
 import pytest
 from qiskit.circuit import Parameter, QuantumCircuit
 from samplomatic import Twirl, build
 
 from ibm_quantum_schemas.models.executor.version_0_1.models import (
+    ChunkPart,
+    ChunkSpan,
     CircuitItemModel,
+    MetadataModel,
     OptionsModel,
     ParamsModel,
     QuantumProgramModel,
@@ -84,8 +89,17 @@ def test_initialization_results_model():
         },
         metadata=None,
     )
-    results = QuantumProgramResultModel(data=[result_item], metadata=None)
+    now = datetime.datetime.now()
+    spans = [
+        ChunkSpan(
+            start=now,
+            stop=now + datetime.timedelta(seconds=5.1),
+            parts=[ChunkPart(idx_item=0, size=5)],
+        )
+    ]
+    metadata = MetadataModel(chunk_timing=spans)
+    results = QuantumProgramResultModel(data=[result_item], metadata=metadata)
 
     assert results.schema_version == "v0.1"
     assert results.data == [result_item]
-    assert results.metadata is None
+    assert results.metadata == metadata
