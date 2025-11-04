9312e3c96fdc0a8789a110032c7566b3cb0693d1
joshuasn <53916441+joshuasn@users.noreply.github.com>
2025-11-04 10:30:08 -0330

    Update ibm_quantum_schemas/models/__init__.py
    Co-authored-by: Ian Hincks <ian.hincks@gmail.com>

--- ibm_quantum_schemas/models/__init__.py
+++ ibm_quantum_schemas/models/__init__.py
@@ -10,4 +10,4 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Models and validation for types used by executor."""
+"""Models"""
