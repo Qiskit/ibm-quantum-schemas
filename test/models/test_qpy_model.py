4664c63485e2d7c1f3f009c1a3838a3ec6d1329f
Ian Hincks <ian.hincks@ibm.com>
2026-01-07 17:37:33 -0500

    Add QpyModelV13ToV17

--- test/models/test_qpy_model.py
+++ test/models/test_qpy_model.py
@@ -16,7 +16,7 @@ import pytest
 from qiskit.circuit import QuantumCircuit
 from samplomatic import ChangeBasis, InjectNoise, Twirl
 
-from ibm_quantum_schemas.models.qpy_model import QpyModelV13ToV16
+from ibm_quantum_schemas.models.qpy_model import QpyModelV13ToV16, QpyModelV13ToV17
 
 
 class TestQpyModelV13ToV16:
@@ -60,3 +60,46 @@ class TestQpyModelV13ToV16:
 
         with pytest.raises(ValueError):
             QpyModelV13ToV16.from_quantum_circuit(circuit, qpy_version)
+
+
+class TestQpyModelV13ToV17:
+    """Tests for ``QpyModelV13ToV17``."""
+
+    @pytest.mark.parametrize("qpy_version", [13, 14, 15, 16, 17])
+    def test_roundtrip(self, qpy_version):
+        """Test that round trips work correctly."""
+        circuit = QuantumCircuit(3)
+        circuit.h(0)
+        circuit.cx(0, 1)
+        circuit.measure_all()
+
+        encoded = QpyModelV13ToV17.from_quantum_circuit(circuit, qpy_version)
+        circuit_out = encoded.to_quantum_circuit()
+
+        assert circuit == circuit_out
+
+    @pytest.mark.parametrize("qpy_version", [15, 16, 17])
+    def test_roundtrip_with_annotations(self, qpy_version):
+        """Test that round trips work correctly for circuits with annotated boxes."""
+        circuit = QuantumCircuit(3)
+        circuit.h(0)
+        circuit.cx(0, 1)
+        with circuit.box([Twirl(), InjectNoise("ref"), ChangeBasis()]):
+            circuit.cx(1, 2)
+        circuit.measure_all()
+
+        encoded = QpyModelV13ToV17.from_quantum_circuit(circuit, qpy_version)
+        circuit_out = encoded.to_quantum_circuit()
+
+        assert circuit == circuit_out
+
+    @pytest.mark.parametrize("qpy_version", [12, 18])
+    def test_unsupported_versions_raise(self, qpy_version):
+        """Test that unsupported versions raise."""
+        circuit = QuantumCircuit(3)
+        circuit.h(0)
+        circuit.cx(0, 1)
+        circuit.measure_all()
+
+        with pytest.raises(ValueError):
+            QpyModelV13ToV17.from_quantum_circuit(circuit, qpy_version)
