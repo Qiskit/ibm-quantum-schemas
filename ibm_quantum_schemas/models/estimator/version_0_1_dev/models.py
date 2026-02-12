c048cb1d0222fd92293a2e42eca84988a042ae7e
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-02-12 16:18:06 +0200

    dynamical decoupling options

--- ibm_quantum_schemas/models/estimator/version_0_1_dev/models.py
+++ ibm_quantum_schemas/models/estimator/version_0_1_dev/models.py
@@ -24,31 +24,54 @@ from ...base_params_model import BaseParamsModel
 from ...qpy_model import QpyModelV13ToV17
 
 
-class TypeTaggedModel(BaseModel):
-    """Base class for type-tagged models."""
-    
-    @model_serializer(mode='wrap')
-    def _serialize_with_type_tag(self, serializer, info):
-        """Wrap serialization with __type__ and __value__."""
-        value = serializer(self)
-        type_name = self.__class__.__name__.replace('Model', '')
-        return {
-            "__type__": type_name,
-            "__value__": value
-        }
-
-
-#class BitArrayValue(TypeTaggedModel):
-#    """The value portion of a BitArray type tag."""
-#    array: Union[list, str]  # Can be tagged ndarray or direct string
-#    num_bits: int
-
-
 class ParamsModel(BaseParamsModel):
     """A model describing the Estimator program inputs."""
 
     schema_version: str = "v0.1"
 
-    
+    # pub: TBD
 
-    # To be continued
+    options: OptionsModel
+    """Options for the Estimator."""
+
+class OptionsModel:
+    dynamical_decoupling: DynamicalDecouplingOptionsModel
+
+
+class DynamicalDecouplingOptionsModel(BaseModel):
+    """Options for dynamical decoupling (DD)."""
+
+    enable: bool = False
+    """Whether to enable DD as specified by the other options in this class."""
+
+    sequence_type: Literal["XX", "XpXm", "XY4"] = "XX"
+    """Which dynamical decoupling sequence to use.
+
+    * ``"XX"``: use the sequence ``tau/2 - (+X) - tau - (+X) - tau/2``
+    * ``"XpXm"``: use the sequence ``tau/2 - (+X) - tau - (-X) - tau/2``
+    * ``"XY4"``: use the sequence
+      ``tau/2 - (+X) - tau - (+Y) - tau (-X) - tau - (-Y) - tau/2``
+    """
+
+    extra_slack_distribution: Literal["middle", "edges"] = "middle"
+    """Where to put extra timing delays due to rounding issues.
+
+    Rounding issues arise because the discrete time step ``dt`` of the system cannot
+    be divided. This option takes following values.
+
+    * ``"middle"``: Put the extra slack to the interval at the middle of the sequence.
+    * ``"edges"``: Divide the extra slack as evenly as possible into intervals at
+      beginning and end of the sequence.
+    """
+
+    scheduling_method: Literal["alap", "asap"] = "alap"
+    """Whether to schedule gates as soon as ("asap") or
+    as late as ("alap") possible.
+    """
+
+    skip_reset_qubits: bool = False
+    """Whether to insert DD on idle periods that immediately follow initialized/reset qubits.
+
+    Since qubits in the ground state are less susceptible to decoherence, it can be beneficial
+    to let them be while they are known to be in this state.
+    """
