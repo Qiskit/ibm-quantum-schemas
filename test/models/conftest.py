27f9d34caf77b82dfc08f9dab20bf001677b3b6e
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-02-16 17:45:40 +0200

    server version is 14

--- test/models/conftest.py
+++ test/models/conftest.py
@@ -33,7 +33,7 @@ def compressed_qpy_circuit() -> str:
     circuit.cx(0, 1)
 
     buffer = BytesIO()
-    qpy_dump(circuit, buffer, version=13)
+    qpy_dump(circuit, buffer, version=14)
     qpy_data = buffer.getvalue()
     compressed = zlib.compress(qpy_data)
     encoded = pybase64.b64encode(compressed).decode("utf-8")
