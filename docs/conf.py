261dc3fb38b03459b282e841cc565d16de416d22
Diego M. Rodríguez <diego@moreda.io>
2026-02-25 14:01:47 +0100

    Document BaseModels in sphinx documentation (#86)
    * Use autodoc_pydantic
    
    * Add executor 0.1 docs
    
    * Tweak gitignore to avoid stubs folder
    
    * Fix URL generation
    
    * Add executor 0.2 docs
    
    * Add noise_learner_v3 0.1 and 0.2 docs
    
    * Add common models
    
    * Fix style
    
    * Customize autosummary class template
    
    * Fix style
    
    * Update ibm_quantum_schemas/models/executor/version_0_1/__init__.py
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    * Reorder tocs
    
    * Apply suggestions from code review
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    * Remove dev models and fix typo
    
    * Add classes header, tweak descriptions
    
    * Fix whitespace
    
    ---------
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ extensions = [
     # This is used by qiskit/documentation to generate links to github.com.
     "sphinx.ext.linkcode",
     "sphinxcontrib.katex",
+    "sphinxcontrib.autodoc_pydantic",
 ]
 templates_path = ["_templates"]
 
@@ -98,10 +99,10 @@ intersphinx_mapping = {
 
 autosummary_generate = True
 autosummary_generate_overwrite = False
-autoclass_content = "both"
+autoclass_content = "class"
 autodoc_typehints = "description"
 autodoc_default_options = {
-    "inherited-members": None,
+    "inherited-members": "BaseModel",
     "show-inheritance": True,
 }
 autodoc_type_aliases = {
@@ -111,6 +112,14 @@ autodoc_type_aliases = {
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
+# Disable jsonschema preview, as some models cannot be serializable.
+autodoc_pydantic_model_show_json = False
+
+# Disable field summary, as it uses the full module name, causing long lines.
+autodoc_pydantic_model_show_field_summary = False
+
+# Disable validator summary, as we are hiding the field summary.
+autodoc_pydantic_model_show_validator_summary = False
 
 # If true, figures, tables and code-blocks are automatically numbered if they
 # have a caption.
@@ -224,4 +233,4 @@ def linkcode_resolve(domain, info):
     else:
         ending_lineno = lineno + len(source) - 1
         linespec = f"#L{lineno}-L{ending_lineno}"
-    return f"https://github.com/Qiskit/ibm-quantum-schemas/tree/{GITHUB_BRANCH}/ibm_quantum_schemas{file_name}{linespec}"
+    return f"https://github.com/Qiskit/ibm-quantum-schemas/tree/{GITHUB_BRANCH}/ibm_quantum_schemas/{file_name}{linespec}"
