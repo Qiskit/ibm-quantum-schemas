b5aa48229fa3c6c2ff45537b472b1b05ac14c8b5
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-02-28 18:18:53 +0200

    starting primitive result model

---
+++ ibm_quantum_schemas/models/estimator/version_0_1_dev/primitive_result_model.py
@@ -0,0 +1,41 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Primitive Result Model."""
+
+
+from __future__ import annotations
+
+from pydantic import BaseModel, Literal
+
+
+class PrimitiveResultModel(BaseModel):
+    """A model describing the Estimator program output."""
+
+    schema_version: Literal["v0.1"] = "v0.1"
+    """Schema version of the results."""
+
+    pub_results: list[PubResultModel]
+    """Result data from the estimator v2 job."""
+
+    metadata: ResultsMetadataModel
+    """Metadata for the estimator v2 job."""
+
+
+class PubResultModel(BaseModel):
+    """A model describing the Estimator program output for a single pub."""
+
+    # data: TBD
+    """Result data from the estimator v2 job."""
+
+    metadata: PubResultMetadataModel
+    """Metadata for the estimator v2 job."""
