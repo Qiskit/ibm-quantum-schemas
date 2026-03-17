a165f5063ca4a55344413dfd50e23a1bba8afe47
Diego M. Rodríguez <diego@moreda.io>
2026-03-17 15:19:50 +0100

    Move estimator models to root

--- ibm_quantum_schemas/models/estimator/version_0_1_dev/__init__.py
+++ ibm_quantum_schemas/models/estimator/version_0_1_dev/__init__.py
@@ -11,3 +11,33 @@
 # that they have been altered from the originals.
 
 """Estimator V0.1 Models and Validation"""
+
+from __future__ import annotations
+
+from ibm_quantum_schemas.models.ndarray_wrapper import NdarrayWrapperModel
+
+from .dynamical_decoupling_options import DynamicalDecouplingOptionsModel
+from .estimator_pub import EstimatorPubModel
+from .execution_options import ExecutionOptionsV2Model
+from .layer_noise_learning_options import LayerNoiseLearningOptionsModel
+from .layer_noise_model import (
+    LayerNoiseModelModel,
+    LayerNoiseModelWrapperModel,
+    PauliLindbladErrorModel,
+    PauliLindbladErrorWrapperModel,
+    PauliListModel,
+    PauliListWrapperModel,
+)
+from .measure_noise_learning_options import MeasureNoiseLearningOptionsModel
+from .noise_learner_results import (
+    NoiseLearnerInputOptionsModel,
+    NoiseLearnerResultsMetadataModel,
+    NoiseLearnerResultsModel,
+)
+from .options import OptionsModel
+from .params import ParamsModel
+from .pec_options import PecOptionsModel
+from .primitive_result import PrimitiveResultWrapperModel
+from .resilience_options import ResilienceOptionsModel
+from .twirling_options import TwirlingOptionsModel, TwirlingStrategyType
+from .zne_options import ExtrapolatorType, ZneOptionsModel
