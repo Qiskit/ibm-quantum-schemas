bf7cd9be6c367f5f9b265b29c750f171716b41fe
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-03-22 21:39:21 +0200

    Don't import NdarrayWrapperModel in the Estimator's init file (#116)
    * Don't import NdarrayWrapperModel in the Estimator's init file
    
    * remove similar imports also in nlv2

--- ibm_quantum_schemas/estimator/version_0_1_dev/__init__.py
+++ ibm_quantum_schemas/estimator/version_0_1_dev/__init__.py
@@ -14,7 +14,6 @@
 
 from __future__ import annotations
 
-from ibm_quantum_schemas.common.ndarray_wrapper import NdarrayWrapperModel
 from ibm_quantum_schemas.estimator.version_0_1_dev.dynamical_decoupling_options import (
     DynamicalDecouplingOptionsModel,
 )
--- test/noise_learner_v2/version_0_1_dev/test_models.py
+++ test/noise_learner_v2/version_0_1_dev/test_models.py
@@ -15,12 +15,12 @@
 import pytest
 from pydantic import ValidationError
 
+from ibm_quantum_schemas.common.typed_qpy_circuit import TypedQpyCircuitModelV13to17
 from ibm_quantum_schemas.noise_learner_v2.version_0_1_dev import (
     LayerNoiseWrapperModel,
     ParamsModel,
     ResultsMetadataModel,
     ResultsModel,
-    TypedQpyCircuitModelV13to17,
 )
 
 
