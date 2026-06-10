76cf2762291067ab93c17409e93920037f5b884b
Samuele Ferracin <sam.ferracin@ibm.com>
2026-06-10 13:56:38 -0400

    Add NLV3 models version `0.3_dev` (#163)
    * added 0.3_dev folder
    
    * added the models
    
    * bug

---
+++ ibm_quantum_schemas/noise_learner_v3/version_0_3_dev/__init__.py
@@ -0,0 +1,50 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+=============================================================================
+NoiseLearnerV3 v0.3 (:mod:`ibm_quantum_schemas.noise_learner_v3.version_0_3_dev`)
+=============================================================================
+
+.. currentmodule:: ibm_quantum_schemas.noise_learner_v3.version_0_3_dev
+
+Models for ``NoiseLearnerV3`` ``v0.3``.
+
+Classes
+=======
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+   ParamsModel
+   OptionsModel
+   PostSelectionOptionsModel
+   NoiseLearnerV3ResultsModel
+   NoiseLearnerV3ResultModel
+   TREXResultPostSelectionMetadataModel
+   TREXResultMetadataModel
+   LinbdbladResultPostSelectionMetadataModel
+   LinbdbladResultMetadataModel
+"""
+
+from ibm_quantum_schemas.noise_learner_v3.version_0_3_dev.models import (
+    LinbdbladResultMetadataModel,
+    LinbdbladResultPostSelectionMetadataModel,
+    NoiseLearnerV3ResultModel,
+    NoiseLearnerV3ResultsModel,
+    OptionsModel,
+    ParamsModel,
+    PostSelectionOptionsModel,
+    TREXResultMetadataModel,
+    TREXResultPostSelectionMetadataModel,
+)
