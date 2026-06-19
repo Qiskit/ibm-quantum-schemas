26df981d4b05cf55d79e7f19974e9c7ba67a5a9a
Anthony <125415978+apchytr@users.noreply.github.com>
2026-06-19 10:04:10 -0400

    Add initial versions Executor v2.0 models (#169)
    * adding executor 2.0 dev files
    
    * Update test/executor/version_2_0_dev/test_models.py
    
    Co-authored-by: Diego M. Rodríguez <diego.plan9@gmail.com>
    
    ---------
    
    Co-authored-by: Diego M. Rodríguez <diego.plan9@gmail.com>

---
+++ ibm_quantum_schemas/executor/version_2_0_dev/__init__.py
@@ -0,0 +1,58 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+===============================================================
+Executor v2.0 (:mod:`ibm_quantum_schemas.executor.version_2_0_dev`)
+===============================================================
+
+.. currentmodule:: ibm_quantum_schemas.executor.version_2_0_dev
+
+Models for ``Executor`` ``v2.0``.
+
+Classes
+=======
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+   ParamsModel
+   QuantumProgramModel
+   CircuitItemModel
+   SamplexItemModel
+   OptionsModel
+   QuantumProgramResultModel
+   QuantumProgramResultItemModel
+   ItemMetadataModel
+   SchedulerTimingModel
+   StretchValueModel
+   MetadataModel
+   ChunkSpan
+   ChunkPart
+"""
+
+from ibm_quantum_schemas.executor.version_2_0_dev.models import (
+    ChunkPart,
+    ChunkSpan,
+    CircuitItemModel,
+    ItemMetadataModel,
+    MetadataModel,
+    OptionsModel,
+    ParamsModel,
+    QuantumProgramModel,
+    QuantumProgramResultItemModel,
+    QuantumProgramResultModel,
+    SamplexItemModel,
+    SchedulerTimingModel,
+    StretchValueModel,
+)
