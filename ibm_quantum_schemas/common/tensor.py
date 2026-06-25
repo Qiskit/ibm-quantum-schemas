2130aeb7811db1579f55c05bb9a282af8f7ca042
Anthony <anthonychytros@gmail.com>
2026-06-25 15:57:17 -0400

    first pass fix

--- ibm_quantum_schemas/common/tensor.py
+++ ibm_quantum_schemas/common/tensor.py
@@ -159,9 +159,12 @@ class CompressedTensorModel(TensorModel):
     @model_validator(mode="after")
     def check_sizes(self):
         """Cross-validate that all sizes are consistent."""
-        raw = zlib.decompress(pybase64.b64decode(self.data))
+        raw = pybase64.b64encode(zlib.decompress(pybase64.b64decode(self.data)))
         elem_size = self._ELEM_SIZE_LOOKUP[self.dtype]
-        if math.ceil(len(raw) / elem_size) != math.prod(self.shape):
+        # each character encodes 6 bits, each set of 4 characters is therefore 3 bytes
+        # finally, suffixed with `=` to force a whole number of bytes.
+        len_data = (len(raw) // 4) * 3 - raw[-2:].count(b"=")
+        if math.ceil(math.prod(self.shape) * elem_size) != len_data:
             raise ValueError(
                 f"Data length {len(raw)} is inconsistent with shape {self.shape} packed at "
                 f"{elem_size} bytes per element."
